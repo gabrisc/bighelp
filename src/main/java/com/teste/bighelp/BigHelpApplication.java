8379912ec74ac198c25c0b14eeb9b06e86025611
gabriel <Gab@27042000>
2021-11-16 01:34:13 -0300

    Initial commit

---
+++ src/main/java/com/teste/bighelp/BigHelpApplication.java
@@ -0,0 +1,13 @@
+package com.teste.bighelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigHelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigHelpApplication.class, args);
+	}
+
+}
